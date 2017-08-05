231f8a88eaaf80b79ed46e0c880b6506a2b47909
Luke Duncan <lukejduncan@gmail.com>
2017-08-05 13:25:30 -0700

    Add skeleton for testing and deployment

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""setup tools control"""
+
+from setuptools import setup
+
+version = '0.0.1'
+reqs = ['numpy']
+
+setup(
+    name='debt-balance',
+    packages=['balancer', 'test'],
+    setup_requires=['flake8'],
+    test_suite='test',
+    entry_points={
+        "console_scripts": ['tfimgsort = tfimgsort.tfimgsort:main']
+        },
+    version=version,
+    description="A tool for evenly sharing the costs of a trip.",
+    long_description="A tool for evenly sharing the costs of a trip",
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Development Status :: 1 - Planning',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Intended Audience :: End Users/Desktop',
+      'License :: Other/Proprietary License',
+      'Programming Language :: Python :: 3 :: Only',
+      'Topic :: Utilities'
+     ],
+    keyword='money vacation trip finance',
+    license='MIT',
+    install_requires=reqs,
+    author="Luke Duncan",
+    author_email="lukejduncan@gmail.com",
+    url='http://www.lukejduncan.com',
+    )
---
+++ test/test_balancer.py
@@ -0,0 +1,7 @@
+import unittest
+
+from balancer.balancer import balance
+
+class TestBalancer(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(False, "It was false")
